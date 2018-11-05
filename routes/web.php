ba10b75605d9478be802fcd04b612a09b777348b
Marco Aurélio <marcoaurelio@MACM.local>
2018-11-05 19:55:19 -0300

    ruta test

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return 'Funcionou!';
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
